9fae3b33483cae97951df6416ae584bd6c23c38c
ECHibiki <ECVerniy@gmail.com>
2019-09-26 16:38:11 +0000

    minor fixes to motd

--- motd.php
+++ motd.php
@@ -1,5 +1,9 @@
 <?php
 	$han = fopen("motd.txt", "w");
+        $_POST['motd'] = preg_replace("/( |^)(http(|s):\/\/.+)( |$)/","<a href='$2'>$2</a>", $_POST['motd']);
+        $_POST['motd'] = str_replace("script","sukuriputo", $_POST['motd']);
+        $_POST['motd'] = str_replace("style","stylu", $_POST['motd']);
+
 	@fwrite($han, $_POST['motd']);
 	fclose($han);
  ?>
