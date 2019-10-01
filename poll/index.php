199a4670714af53440528d7e0711aaeb72b3a222
ECHibiki <ECVerniy@gmail.com>
2019-10-01 00:19:20 +0000

    gz compression

---
+++ poll/index.php
@@ -0,0 +1,345 @@
+<?php
+	$expresion = '/(C:\\\xampp\\\htdocs\\\[a-zA-Z]+|\/var\/www\/html\/[a-zA-Z]+)/';
+	while(preg_match("$expresion", getcwd())){
+		chdir ('../');
+	}
+	require_once ('inc/bans.php');
+	require_once ('inc/functions.php');
+	loadConfig();
+	
+	if($config['ban_block']){
+		if(!isset($t2)){
+			$t1 = explode(" ", microtime())[0];
+			checkDNSBL();
+			checkBan();
+			$t2 = explode(" ", microtime())[0];
+		}
+		else{
+			echo ($t2 - $t1);
+		}
+	}
+
+?><!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+        <script type="text/javascript">
+	  var
+                        active_page = "index"
+            , board_name = "poll";
+	          </script>
+
+			<link rel="stylesheet" media="screen" href="/stylesheets/style.css">
+		<link rel="shortcut icon" href="/static/favicon.png">		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+				<link rel="stylesheet" type="text/css" id="stylesheet" href="/stylesheets/Dark-kissu.css">		<link rel="stylesheet" href="/stylesheets/font-awesome/css/font-awesome.min.css">		<link rel="stylesheet" href="/static/flags/flags.css">		<script type="text/javascript">
+			var configRoot="/";
+			var inMod = false;
+			var modRoot="/"+(inMod ? "mod.php?/" : "");
+		</script>
+					<script type="text/javascript" src="/main.js"></script>
+								<title>/poll/ - polling and honesty</title>
+
+</head>
+<body class="8chan vichan is-not-moderator active-index" data-stylesheet="Dark-kissu.css">
+<div id="uppercontents">
+	<div id="subuppercontents">
+		<div class="boardlist"><span class="sub" data-description="0">[ <a href="/">home</a> / <a href="/agg/">agg</a> / <a href="/all/">all</a> ]</span>  <span class="sub" data-description="1">[ <a href="/b/">g</a> / <a href="/qa/">qa ]</a> / <a href="/megu/">megu</a> ]</span>  <span class="sub" data-description="2">[ <a href="https://theatre.kissu.moe">sync</a> / <a href="https://ban.kissu.moe">text</a> ]</span>  <span class="sub" data-description="3">[ <a href="https://himasugi.blog/jp/">hima</a> / <a href="https://boards.4channel.org/qa/">qa4</a> / <a href="https://github.com/ECHibiki/ViQa-Kissu/">ViQa</a> ]</span></div><script type='text/javascript'>if (typeof do_boardlist != 'undefined') do_boardlist();</script>
+	<a name="top"></a>
+			<img id="bannerimg" class="board_image" src="/static/banners/banner-kissu-4.gif" alt="" />
+<script>
+document.getElementById("bannerimg").onclick = function(){
+	
+        var request = new XMLHttpRequest(); 
+        var motd = "";
+        request.open("GET", '/motd.txt');
+                request.onreadystatechange = function() { 
+                if (this.readyState === 4 && this.status === 200) {
+                        motd = this.responseText;
+                        var request = new XMLHttpRequest(); 
+                        localStorage.firstLoad = 3;
+                        alert(
+                        "<h1>Welcome to kissu.moe!</h1><br/>\
+                         <h2>Message Of the Day</h2>\
+                        <p><strong>" + motd + "</strong></p>\
+                        <h2>Boards</h2>\
+                        <ul style='text-align: left;'><li>/qa/ - Random content(NSFW spoilered)</li><li>/b/ - Site Developement</li><li>/megu/ - NSFW content</li></ul>\
+                        <h2>Select Default Theme</h2>\
+                        Other options are selectable later in options<br/><br/>\
+                        <label>Default Theme: <select onchange='$(\"#style-select-\" + $(this).val()).click();'><option value='1'>Light</option><option selected='selected' value='2'>Dark</option><option value='3'>Special</option></select></label><br/>\
+                        <h2>Rules</h2>\
+                        <p>Don't post obnoxious stuff. Bans are only reserved for the worst cases of people. A deletion does not mean it's personal</p><hr/>\
+                        <br/>Contact Vermin for issues, site bugs and feedback</p>\
+                        "
+                        );
+
+                };
+
+        }
+        request.send();
+}
+</script>
+
+
+	<header>
+	<h1 style="padding-top:10px;">/poll/ - polling and honesty</h1>
+	<div class="subtitle">
+									use for asking the community
+								</div>
+	</header>
+<h1>New Thread</h1>
+	<div id="topcontainer">
+	
+	
+
+	<div>
+	                                        <form class="form_submit" name="post" onsubmit="return dopost(this);" enctype="multipart/form-data" action="/post.php" method="post">
+<input  type="hidden"  value=""  name="lastname">
+<div style="display:none"><input type="text" name="user" value=""></div>
+<input type="hidden" name="board" value="poll">
+<input style="display:none" type="text" value="83885" name="firstname">
+	<input type="hidden" name="page" value="1">
+	<table>
+		<tr id="namerow" style="display:none">
+			<th>
+				Name				<input style="display:none" type="text" value="Q#50Z?IvfwFV/4x6.S8⚛R%2P3LlqE&lt;yajXt_|&gt;z☄{⛨K☌*']N$B;☎9@Mo-&amp;u♉:sh" name="pq96kxrw♍n0y">
+			</th>
+			<td>
+				<input type="text" name="name" size="25" maxlength="35" autocomplete="off"> 				<input type="hidden" value="⚩v[S3|'pBab8G⛰r}A.%icq1fk:l ⚧LwM`?TY!uRN@⛏no⛋*s){UW j⛯&gt;He♙h" name="message">
+			</td>
+		</tr>		<tr>
+			<th>
+				Options				<textarea style="display:none" name="username">Q3V1 )☼dMU(&lt;g$T⛢w;klN=5/FHS%pX_7zn]0sbA&gt;G\IYLj♂{2Zu,}⛊⛦K-r x:ta~E.moc6y@h8e♸[C&amp;⛎JB#qO|i^'+`49v☢</textarea>
+			</th>
+			<td>
+								<input type="text" name="email" size="25" maxlength="40" autocomplete="off" id='option_input'>
+<input class="form_submit" id="email_submit" accesskey="s" style="margin-left:2px;" type="submit" name="post" value="New Topic" />
+								<input style="display:none" type="text" name="search" value="E!Yxz)H \B=5 ]`&gt;☒k{?⚥C⛇QT-q*Ujd⛟PF1iA♴I⚝68h~sfb.OcmM%Kgr@/nNtGJ$X♩_⛌':">
+			
+			
+				<input type="hidden" value="☠♯QNt%⛬jv&lt;)5asJ⚋hu⚐=⚣}k(R-i,W/GHx" name="text">
+			</td>
+		</tr>		<tr id="subjectrow" style="display:none">
+			<th>
+				Subject				<input name="q" value="D$_TQ49vnS|(YwK*" type="hidden">
+			</th>
+			<td>
+				<input style="" type="text" name="subject" size="25" maxlength="100" autocomplete="off">
+<input class="form_submit" id="subject_submit"  accesskey="s" style="margin-left:2px;" type="submit" name="post" value="New Topic" />
+			</td>
+		</tr>
+				<tr>
+			<th>
+				Comment				<input type="hidden" value="!k⚠Oe:BR⚩1w]^qaFTlpU&amp;myGivH+2r*n⚧60Kgx'SP&gt;j☊[♳ 3AENCs;{#\Wc8 ⛵⚲$Z_J}☕oV7" name="3xksl2jcz8q7wr♒opeht⛦bg⛽51dumi">
+			</th>
+			<td>
+				<textarea name="body" id="body" rows="3" cols="40"></textarea>
+				<div   style="display:none"><input   type="text"   name="e⛇2⛥81☗azhd7fkpvnctj5bu6i"   value="`R$'☔♦sNt&gt;1l %)F⚷?O-j^95yza,wb6h♹u0q!Tp7Kgv/c{:X;(3E&amp;DrH@|#_Z"></div>
+				
+			</td>
+		</tr>
+										<tr id="upload">
+			<th>
+				File			</th>
+			<td>
+				<input type="file" name="file" id="upload_file" style="width: 190px;"> 
+				<script type="text/javascript">if (typeof init_file_selector !== 'undefined') init_file_selector(1);</script>
+<button onclick="
+$('input[id=upload_file]').val('');
+return false;
+" id="remove_img">X</button>
+									<div style="float:none;text-align:left" id="upload_url">
+						<label for="file_url">Or URL: </label>
+						<input style="display:inline" type="text" id="file_url" name="file_url" size="35">
+					</div>
+								
+			</td>
+		</tr>
+		
+		
+		<tr id="additional_clicker"><td colspan="2"><button onclick="
+visible = $('tr[id=namerow]').css('display') == 'table-row';
+if(!visible){
+	$('tr[id=namerow]').css('display', 'table-row');
+	$('tr[id=subjectrow]').css('display', 'table-row');
+	$('input[id=email_submit]').css('display', 'none');
+	$('input[id=subject_submit]').css('display', 'table-row');
+	$('tr[id=embedrow]').css('display','table-row');
+	$('tr[id=advancedoptrow]').css('display', 'table-row');
+	$('tr[id=captcharow]').css('display', 'table-row');
+	$('tr[id=spoilerrow]').css('display', 'table-row');
+        $('tr[id=pswrdrow]').css('display', 'table-row');
+this.textContent = '[Remove Hidden Options]';
+}
+else{
+	$('tr[id=namerow]').css('display', 'none');
+	$('tr[id=subjectrow]').css('display', 'none');
+	$('input[id=email_submit]').css('display', 'table-row');
+	$('input[id=subject_submit]').css('display', 'none');
+	$('tr[id=embedrow]').css('display','none');
+	$('tr[id=advancedoptrow]').css('display', 'none');
+	$('tr[id=captcharow]').css('display', 'none');
+	$('tr[id=spoilerrow]').css('display', 'none');
+	$('tr[id=pswrdrow]').css('display', 'none');
+this.textContent = '[View Hidden Options]';
+}
+return false;
+">[View Hidden Options]</a></td></tr>
+
+
+		<tr  style="display:none" id="embedrow">
+			<th>
+				Video Stream Embedding				
+			</th>
+			<td>
+				<input type="text" name="embed" value="" size="30" maxlength="120" autocomplete="off">
+			</td>
+		</tr>
+<tr id="advancedoptrow" style="display:none"><th>Advanced Options</th><td>
+<label><input type="checkbox" id="force_noko" onchange="
+localStorage.AlwaysNoko = this.checked;
+if(this.checked == true)
+        $('input[id=option_input]').val($('input[id=option_input]').val() + ' noko');
+">Always Noko</label>
+<label><input type="checkbox" id="force_sage" onchange="
+localStorage.AlwaysSage = this.checked;
+if(this.checked == true)
+	$('input[id=option_input]').val($('input[id=option_input]').val() + ' sage');
+">Always Sage</label>
+
+<script>
+if(localStorage.AlwaysSage == "true")
+        $('input[id=option_input]').val($('input[id=option_input]').val() + ' sage');
+$('input[id=force_sage]').prop('checked', localStorage.AlwaysSage == "true");
+
+if(localStorage.AlwaysNoko == "true")
+        $('input[id=option_input]').val($('input[id=option_input]').val() + ' noko');
+$('input[id=force_noko]').prop('checked', localStorage.AlwaysNoko == "true");
+
+</script>
+
+				<select id="option_simplifier" autocomplete="off">
+					<option value="">Options</option>
+					<option value="repo">Repost</option>
+					<option value="sage">Hold Back</option>
+					<option value="noko">Remain</option>				</select> 
+</td></tr>
+<tr id="captcharow"  style="display:none"><th>Captcha Type</th><td>				<span name='captchasel'>
+				<label><input type="radio" name="captype" class="rec" value="recaptcha"> Recaptcha </label>
+				<label><input type="radio" name="captype" class="cap" value="captchouli"> Captchouli </label>
+				</span>
+</td>
+<tr id="spoilerrow"  style="display:none"><th>Spoiler</th><td> <input id="spoiler" name="spoiler" type="checkbox"> <label for="spoiler">Spoiler Image</label></td></tr>
+				<tr id="pswrdrow" style="display: none;" >
+			<th>
+				Password				
+			</th>
+			<td >
+				<input type="text" name="pswrd" value="" size="12" maxlength="18"> 
+				<span class="unimportant">(For file deletion.)</span>
+				
+			</td>
+		</tr>		<tr><td colspan=2><span name='markup-hint' style='font-size:10px'>Markup tags exist for bold, itallics, header, spoiler etc. as listed in " [options] > View Formatting "</span></td></tr>
+	</table>
+
+<input type="hidden" name="hash" value="a9c46a012ca3f1de2956bb3ebb43171a6f8ed74c">
+</form>
+
+<script type="text/javascript">
+	rememberStuff();
+</script>
+                
+		</div>
+	</div>
+	</div>
+	</div>
+		<div id="lowercontents">
+	<hr /><div class="blotter">Site will be unstable at 20:05<hr/>Proud part of the /qa/ webring with<a style="" href="http://4taba.net/"> http://4taba.net/</a>,<a href="https://qa.booru.org/">https://qa.booru.org/</a> and our redacted chatrooms</div>	<hr />
+			
+	<span id="thread-links-top">
+		<a id="thread-return-top" href="">[Refresh]</a>
+		<a id="thread-bottom" href="#bottom">[Bottom]</a>
+							<a id="thread-catalog-top" href="/poll/catalog">[Catalog]</a>
+				<a id="archive-link-top" href="/poll/archive/">[Archive]</a>
+	</span>
+	<br/><hr/>
+	
+	
+	<form name="postcontrols" action="/post.php" method="post">
+	<input type="hidden" name="board" value="poll" />
+		
+	<div id="post-moderation-fields">
+		<div id="delete-fields">
+		Delete Post [<input title="Delete file only" type="checkbox" name="file" id="delete_file" />
+				 <label for="delete_file">File</label>] <label for="password">Password</label> 
+		<input id="pswrd" type="text" name="pswrd" size="12" maxlength="18" />
+<script>document.getElementById("pswrd").value = localStorage.getItem("pswrd");</script>
+		<input type="submit" name="delete" value="Delete" />
+	</div>
+		
+	<div id="report-fields">
+		<label for="reason">Reason</label> 
+		<input id="reason" type="text" name="reason" size="20" maxlength="30" />
+		<input type="submit" name="report" value="Report" />
+	</div>
+</div>
+	
+	
+			<span id="thread-links">
+				<a id="thread-return" href="">[Refresh]</a>
+				<a id="thread-top" href="#top">[Top]</a>
+                							<a id="thread-catalog" href="/poll/catalog">[Catalog]</a>
+		                <a id="archive-link-bottom" href="/poll/archive/">[Archive]</a>
+
+			</span>
+			
+			<span id="thread-quick-reply">
+				<a id="link-quick-reply" href="#">[Post a Reply]</a>
+			</span>
+	</form>
+	<a name="bottom"></a>
+	<div class="boardlist bottom"><span class="sub" data-description="0">[ <a href="/">home</a> / <a href="/agg/">agg</a> / <a href="/all/">all</a> ]</span>  <span class="sub" data-description="1">[ <a href="/b/">g</a> / <a href="/qa/">qa ]</a> / <a href="/megu/">megu</a> ]</span>  <span class="sub" data-description="2">[ <a href="https://theatre.kissu.moe">sync</a> / <a href="https://ban.kissu.moe">text</a> ]</span>  <span class="sub" data-description="3">[ <a href="https://himasugi.blog/jp/">hima</a> / <a href="https://boards.4channel.org/qa/">qa4</a> / <a href="https://github.com/ECHibiki/ViQa-Kissu/">ViQa</a> ]</span></div>
+	<div class="pages">
+		Previous 		 [<a class="selected">1</a>] 		 Next
+					 | <a href="/poll/catalog">Catalog</a>
+			</div>
+	
+
+	
+
+	<footer>
+		<p class="unimportant" style="margin-top:20px;text-align:center;">- Tinyboard + 
+			<a href="https://engine.vichan.net/">vichan</a> 5.1.4 -
+		<br>Tinyboard Copyright &copy; 2010-2014 Tinyboard Development Group
+		<br><a href="https://engine.vichan.net/">vichan</a> Copyright &copy; 2012-2018 vichan-devel</p>
+
+		<p class="unimportant" style="text-align:center;">All trademarks, copyrights, comments, and images on this page are owned by and are the responsibility of their respective parties.</p><p class="unimportant" style="text-align:center;">Concerns to the gmail of ECVerniy</p>	</footer>
+	<script type="text/javascript">
+		ready();
+	</script>
+	</div>
+</body>
+</html>
+<?php
+	$expresion = '/(C:\\\xampp\\\htdocs\\\[a-zA-Z]+|\/var\/www\/html\/[a-zA-Z]+)/';
+	while(preg_match("$expresion", getcwd())){
+		chdir ('../');
+	}
+	require_once ('inc/bans.php');
+	require_once ('inc/functions.php');
+	loadConfig();
+	
+	if($config['ban_block']){
+		if(!isset($t2)){
+			$t1 = explode(" ", microtime())[0];
+			checkDNSBL();
+			checkBan();
+			$t2 = explode(" ", microtime())[0];
+		}
+		else{
+			echo ($t2 - $t1);
+		}
+	}
+
+?>
