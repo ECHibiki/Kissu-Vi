b20f850a1ce1b11a076f329b37bb5fa32c38b6ba
ECHibiki <ecverniy@gmail.com>
2020-03-26 00:06:46 -0400

    undo some testing code

--- poll.php
+++ poll.php
@@ -4,7 +4,6 @@ require_once 'inc/functions.php';
 require_once 'inc/polling.php';
 
 if(isset($_POST['respond_poll'])){
-	var_dump($_POST);echo"<br/>";
 	$error = Polling::respondToPoll($_POST['id'], $_POST['response_json']);
 	if($error)
 		echo json_encode(array("error"=>$error));
