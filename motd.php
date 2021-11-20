93d274f6542bd29710c7809738aa46e947eb76a2
ECHibiki <ecverniy@gmail.com>
2021-11-19 23:47:48 -0500

    fix security issues

--- motd.php
+++ motd.php
@@ -2,11 +2,14 @@
 	if(isset($_POST['motd'])){
 	$han = fopen("motd.txt", "w");
         $_POST['motd'] = preg_replace("/( |^)(http(|s):\/\/.+)( |$)/","<a href='$2'>$2</a>", $_POST['motd']);
-        $_POST['motd'] = str_replace("script","sukuriputo", $_POST['motd']);
-        $_POST['motd'] = str_replace("style","stylu", $_POST['motd']);
-        $_POST['motd'] = str_replace("style","stylu", $_POST['motd']);
-        $_POST['motd'] = str_replace("onload","onclick", $_POST['motd']);
-        $_POST['motd'] = str_replace("onready","onclick", $_POST['motd']);
+        $_POST['motd'] = preg_replace("/script/i","sukuriputo", $_POST['motd']);
+        $_POST['motd'] = preg_replace("/style/i","stylo", $_POST['motd']);
+        $_POST['motd'] = preg_replace("/onload/i","onloud", $_POST['motd']);
+        $_POST['motd'] = preg_replace("/onready/i","onclock", $_POST['motd']);
+        $_POST['motd'] = preg_replace("/document\./i","document,", $_POST['motd']);
+        $_POST['motd'] = preg_replace("/window\./i","window,", $_POST['motd']);
+        $_POST['motd'] = preg_replace("/global\./i","global,,", $_POST['motd']);
+        $_POST['motd'] = preg_replace("/xml/i","markup", $_POST['motd']);
 
 	fwrite($han, $_POST['motd']);
 	fclose($han);
